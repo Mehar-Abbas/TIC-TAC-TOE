565e12228e2a07460864e284204f7a30c6e741c5
Mehar Abbas <mabbas@my.waketech.edu>
2020-12-12 15:32:42 -0500

    working on local

--- app.js
+++ app.js
@@ -1,23 +1,56 @@
-
+const path = require("path")
 const http = require("http")
 const fs = require("fs")
 
-const port = 8080
+const port = 3000
+//
+const server = http.createServer((req, res) => {
 
-const server = http.createServer(function (req, res){
-    res.writeHead(200, {"Content-Type":"text/html"})
-    fs.readFile("index.html", function (error, data){
-        if (error) {
-            res.writeHead(404)
-            res.write("Error: file not found")
-        }  else {
-            res.write(data)
-        }
-        res.end()
+    let filePath = path.join(__dirname, "public", req.url === "/" ? "index.html" : req.url)
+
+    let extension_name = path.extname(filePath)
+
+    let content_type = "text/html"
+
+    switch (extension_name) {
+        case ".css" : content_type = "text/css";
+            break;
+        case ".js" : content_type = "text/javascript";
+            break;
+        case ".json": content_type = "application/json";
+            break;
+    }
+
+    fs.readFile(filePath, (err, data) => {
+       res.writeHead(200, {"Content-Type": content_type })
+        res.end(data, "utf8")
+
+
     })
-})
 
-server.listen(port, function (error) {
+})
+//-----------------------------------------------------------------------------------
+
+    // // extension
+    //
+    //
+    // res.writeHead(200, {"Content-Type":"text/html"})
+    //
+    // fs.readFile("index.html", "utf8", function (error, data){
+    //     if (error) {
+    //         res.writeHead(404)
+    //         res.write("Error: file not found")
+    //     }  else {
+    //         res.write(data)
+    //     }
+    //     res.end()
+    // })
+    // //
+
+//---------------------------------------
+
+
+server.listen( port, function (error) {
     if(error) {
         console.log("something went wrong", error)
     } else{
