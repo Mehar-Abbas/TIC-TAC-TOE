a18721c18642c6b0f076f272441c579a7e5cc774
Mehar Abbas <mabbas@my.waketech.edu>
2020-12-12 23:57:19 -0500

    js working-2

--- public/scripts.js
+++ public/scripts.js
@@ -1,30 +1,51 @@
 
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
-
+const WINNING_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
 const cellElements = document.querySelectorAll('[data-cell]')
 let circlesTurn
 
-// startGame()
-//
-// function startGame() {
-//     circlesTurn = false
+startGame()
+
+function startGame() {
+    circlesTurn = false
 cellElements.forEach(cell => {
         cell.addEventListener('click', afterClick, {once: true})
     })
-// }
+}
 
 function afterClick(e) {
     const cell = e.target
     const currentClass = circlesTurn ? CIRCLE_CLASS : X_CLASS
     putMark(cell, currentClass)
-    // swapTurn()
+    if (checkforWin(currentClass)) {
+        console.log('winner')
+    }
+    swapTurn()
 
 }
 function putMark(cell, currentClass) {
     cell.classList.add(currentClass)
 }
-//
-// function swapTurn() {
-//     circlesTurn = !circlesTurn
-// }
+
+function swapTurn() {
+    circlesTurn = !circlesTurn
+}
+
+function checkforWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every((index => {
+            return cellElements[index].classList.contains(currentClass)
+        }))
+    })
+
+}
